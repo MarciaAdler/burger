ab994ba652a8404f697ba3935d55a132239868bc
Marcia <marciaadler718@gmail.com>
2020-02-11 16:14:10 -0500

    added orm, controller and model code

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,39 @@
 var connection = require("../config/connection.js");
 // create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 //selectAll()
+
+var orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(table, col, val, cb) {
+    var query = `INSERT INTO ${table}(${col}) ("${val}")`;
+    console.log(query);
+    connectionl.query(query, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  updateOne: function(table, col, condition, cb) {
+    var query = `UPDATE ${table} SET ${col} = true WHERE ${condition}`;
+
+    console.log(query);
+    connection.query(query, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
 //insertOne()
 //updateOne()
 // Export the ORM object in module.exports.
--- models/burger.js
+++ models/burger.js
@@ -2,4 +2,21 @@
 var orm = require("../config/orm.js");
 // create the code that will call the ORM functions using burger specific input for the ORM.
 
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(col, condition, cb) {
+    orm.updateOne("burgers", col, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 // export at the end of the burger.js file
