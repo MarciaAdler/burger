73afa265dac5afe742f6681687b54e7e6eef394a
Marcia <marciaadler718@gmail.com>
2020-02-12 20:18:11 -0500

    added connection code for heroku to database

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@
 // Export connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Databasepassword",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDM_URL) {
+  connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Databasepassword",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
